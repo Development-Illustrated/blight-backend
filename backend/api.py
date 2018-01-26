0db77f016a4aab963966d7339959f022ec626b51
Adam <adamprobert@live.co.uk>
2018-01-26 20:32:35 +0000

    First commit

---
+++ backend/api.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route("/api")
+def hello():
+    return "Hello GGJ2018!"
+
+
+@app.route('/api/authenticate', methods=['GET', 'POST'])
+def add_message(uuid):
+    content = request.get_json(silent=True)
+    print(content)
+    return uuid
+
+
+if __name__ == '__main__':
+    app.run()
+
