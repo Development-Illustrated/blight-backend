a0dcb744372929df235fb39a999fd730f52c3c68
Adam <adamprobert@live.co.uk>
2018-01-27 15:03:42 +0000

    Google landmark query working

---
+++ backend/landmarks/google_places.py
@@ -0,0 +1,53 @@
+import requests
+from pymongo import MongoClient
+
+apikey = "AIzaSyDZEOKPfu1-yvYeSSuT8-cpDgnAKTtjKLk"
+db = MongoClient().get_database("blight")
+
+def parse_location(local):
+    name = local["name"]
+    latlong = str(local["geometry"]["location"]["lat"]) + ',' + str(local["geometry"]["location"]["lng"])
+    icon = local["icon"]
+
+    retjson = {
+        "name":name,
+        "coords":latlong,
+        "icon":icon,
+        "ownershipTeam":None,
+        "inventoryItems":[],
+        "scheme": "",
+        "status":"",
+        "degradeRate":5
+
+    }
+    return retjson
+
+
+def find_places(location = '51.481581,-3.179090', radius=500):
+    params = {'location': location, "radius": radius, "type": "points_of_interest", "key": apikey}
+    resp = requests.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', params=params)
+    print("Querying google api for places.. \nRequest:" + resp.url)
+
+    data = resp.json()["results"]
+
+    for local in data:
+        deets = parse_location(local)
+        store_in_db(deets)
+
+
+
+
+def store_in_db(local):
+
+    out = db.landmarks.remove({"name":local["name"]})
+    if out["n"]:
+        print("Removed " + local["name"])
+
+    db.landmarks.insert(local)
+    print("Inserted " + local["name"] + " into landmarks collection")
+
+
+
+
+
+
